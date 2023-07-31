f356eb30b05580c8c72f32d3a0c4d19645dd1ce9
rahulJ-addweb <rahul@addwebsolution.in>
2023-07-31 18:02:04 +0530

    artist crud operations

--- src/Queries/Master/FilterArtist.php
+++ src/Queries/Master/FilterArtist.php
@@ -16,6 +16,14 @@ class FilterArtist extends BaseFilter
     public function __invoke($query, $input)
     {
         $arguments = $this->getFilterParams($input);
-        return $query->where('artist_name', 'like', '%' . urldecode($arguments['artist_name']) . '%');
+        if(!empty($arguments['artist_name']) && !empty($arguments['artist_type'])) {
+            return $query->where('artist_name', 'like', '%' . urldecode($arguments['artist_name']) . '%')->where('artist_type', '=', $arguments['artist_type']);
+        }
+        if(isset($arguments['artist_name'])) {
+            return $query->where('artist_name', 'like', '%' . urldecode($arguments['artist_name']) . '%');
+        }
+        if(!empty($arguments['artist_type'])) {
+            return $query->where('artist_type', '=', $arguments['artist_type']);
+        }
     }
 }
