afd82a11169085fc41e59b7718e4620dd9c7f0fd
rahulJ-addweb <rahul@addwebsolution.in>
2023-09-04 18:59:46 +0530

    download/email ticket & invoice

--- src/Mutations/Shop/ThankYouScreen/ThankYouScreenMutation.php
+++ src/Mutations/Shop/ThankYouScreen/ThankYouScreenMutation.php
@@ -13,6 +13,7 @@ use Webkul\Product\Repositories\TicketOrderRepository;
 use Webkul\Product\Repositories\ProductRepository;
 use Webkul\Customer\Repositories\CustomerAddressRepository;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+use App\Events\SendEventTicket;
 
 class ThankYouScreenMutation extends Controller
 {
@@ -94,5 +95,224 @@ class ThankYouScreenMutation extends Controller
             $product['paymentMethod'] = 'Credit Card';
         }
         return $product;
+    }
+
+    public function downloadTicket($rootValue, array $args, GraphQLContext $context)
+    {
+        $product = $this->productRepository->findOrFail($args['product_id']);
+        $customer = bagisto_graphql()->guard($this->guard)->user();
+        if(!empty($customer)) {
+            $product['customerFirstName'] = $customer->first_name ?? null;
+            $product['customerLastName'] = $customer->last_name ?? null;
+            $product['customerEmail'] = $customer->email ?? null;
+            $product['customerPhone'] = $customer->phone ?? null;
+            $customerAddress = $this->customerAddressRepository->where([["customer_id", $customer->id], ['default_address', 1]])->first();
+            if(!empty($customerAddress)){
+                $product['customerAddressFirstName'] = $customerAddress['first_name'];
+                $product['customerAddressLastName'] = $customerAddress['last_name'];
+                $product['customerAddressEmail'] = $customerAddress['email'];
+                $product['customerAddress1'] = $customerAddress['address1'];
+                $product['customerAddress2'] = $customerAddress['address2'];
+                if($customer->phone == null) {
+                    $product['customerPhone'] = $customerAddress['phone'] ?? null;
+                }
+                $product['customerAddressCity'] = $customerAddress['city'];
+                $product['customerAddressState'] = $customerAddress['state'];
+                $product['customerAddressCountry'] = $customerAddress['country'];
+                $product['customerAddressPostCode'] = $customerAddress['postcode'];
+            }
+        }
+
+        $result = DB::table('orders')
+            ->leftJoin('cart_items', 'cart_items.cart_id', '=', 'orders.cart_id')
+            ->leftJoin('products', 'cart_items.product_id', '=', 'products.id')
+            ->addSelect('cart_items.quantity', 'orders.created_at', 'orders.id AS order_id')
+            ->where('products.type', 'booking')
+            ->where('orders.status', 'completed')
+            ->groupBy('cart_items.product_id')->first();
+
+        if(isset($result)) {
+            $orderPlacedOn = null;
+            if(isset($result->created_at))
+                $orderPlacedOn = date("F d, Y, H:i", strtotime($result->created_at));
+
+            $orderId = null;
+            if(isset($result->order_id))
+                $orderId = "#".$result->order_id;
+
+            $product['noOfTickets'] = $result->quantity ?? 0;
+            $product['orderId'] = $orderId;
+            $product['order_id'] = $result->order_id;
+            $product['orderPlacedOn'] = $orderPlacedOn;
+            $product['paymentMethod'] = 'Credit Card';
+        }
+
+        $url = $this->productRepository->downloadTicket($product);
+        $response['url'] = $url;
+        return $response;
+    }
+    public function downloadInvoice($rootValue, array $args, GraphQLContext $context)
+    {
+        $product = $this->productRepository->findOrFail($args['product_id']);
+        $customer = bagisto_graphql()->guard($this->guard)->user();
+        if(!empty($customer)) {
+            $product['customerFirstName'] = $customer->first_name ?? null;
+            $product['customerLastName'] = $customer->last_name ?? null;
+            $product['customerEmail'] = $customer->email ?? null;
+            $product['customerPhone'] = $customer->phone ?? null;
+            $customerAddress = $this->customerAddressRepository->where([["customer_id", $customer->id], ['default_address', 1]])->first();
+            if(!empty($customerAddress)){
+                $product['customerAddressFirstName'] = $customerAddress['first_name'];
+                $product['customerAddressLastName'] = $customerAddress['last_name'];
+                $product['customerAddressEmail'] = $customerAddress['email'];
+                $product['customerAddress1'] = $customerAddress['address1'];
+                $product['customerAddress2'] = $customerAddress['address2'];
+                if($customer->phone == null) {
+                    $product['customerPhone'] = $customerAddress['phone'] ?? null;
+                }
+                $product['customerAddressCity'] = $customerAddress['city'];
+                $product['customerAddressState'] = $customerAddress['state'];
+                $product['customerAddressCountry'] = $customerAddress['country'];
+                $product['customerAddressPostCode'] = $customerAddress['postcode'];
+            }
+        }
+
+        $result = DB::table('orders')
+            ->leftJoin('cart_items', 'cart_items.cart_id', '=', 'orders.cart_id')
+            ->leftJoin('products', 'cart_items.product_id', '=', 'products.id')
+            ->addSelect('cart_items.quantity', 'orders.created_at', 'orders.id AS order_id')
+            ->where('products.type', 'booking')
+            ->where('orders.status', 'completed')
+            ->groupBy('cart_items.product_id')->first();
+
+        if(isset($result)) {
+            $orderPlacedOn = null;
+            if(isset($result->created_at))
+                $orderPlacedOn = date("F d, Y, H:i", strtotime($result->created_at));
+
+            $orderId = null;
+            if(isset($result->order_id))
+                $orderId = "#".$result->order_id;
+
+            $product['noOfTickets'] = $result->quantity ?? 0;
+            $product['orderId'] = $orderId;
+            $product['order_id'] = $result->order_id;
+            $product['orderPlacedOn'] = $orderPlacedOn;
+            $product['paymentMethod'] = 'Credit Card';
+        }
+
+        $url = $this->productRepository->downloadTicket($product);
+        $response['url'] = $url;
+        return $response;
+    }
+    public function emailEventTicket($rootValue, array $args, GraphQLContext $context)
+    {
+        $product = $this->productRepository->findOrFail($args['product_id']);
+        $customer = bagisto_graphql()->guard($this->guard)->user();
+        SendEventTicket::dispatch($customer);
+        if(!empty($customer)) {
+            $product['customerFirstName'] = $customer->first_name ?? null;
+            $product['customerLastName'] = $customer->last_name ?? null;
+            $product['customerEmail'] = $customer->email ?? null;
+            $product['customerPhone'] = $customer->phone ?? null;
+            $customerAddress = $this->customerAddressRepository->where([["customer_id", $customer->id], ['default_address', 1]])->first();
+            if(!empty($customerAddress)){
+                $product['customerAddressFirstName'] = $customerAddress['first_name'];
+                $product['customerAddressLastName'] = $customerAddress['last_name'];
+                $product['customerAddressEmail'] = $customerAddress['email'];
+                $product['customerAddress1'] = $customerAddress['address1'];
+                $product['customerAddress2'] = $customerAddress['address2'];
+                if($customer->phone == null) {
+                    $product['customerPhone'] = $customerAddress['phone'] ?? null;
+                }
+                $product['customerAddressCity'] = $customerAddress['city'];
+                $product['customerAddressState'] = $customerAddress['state'];
+                $product['customerAddressCountry'] = $customerAddress['country'];
+                $product['customerAddressPostCode'] = $customerAddress['postcode'];
+            }
+        }
+
+        $result = DB::table('orders')
+            ->leftJoin('cart_items', 'cart_items.cart_id', '=', 'orders.cart_id')
+            ->leftJoin('products', 'cart_items.product_id', '=', 'products.id')
+            ->addSelect('cart_items.quantity', 'orders.created_at', 'orders.id AS order_id')
+            ->where('products.type', 'booking')
+            ->where('orders.status', 'completed')
+            ->groupBy('cart_items.product_id')->first();
+
+        if(isset($result)) {
+            $orderPlacedOn = null;
+            if(isset($result->created_at))
+                $orderPlacedOn = date("F d, Y, H:i", strtotime($result->created_at));
+
+            $orderId = null;
+            if(isset($result->order_id))
+                $orderId = "#".$result->order_id;
+
+            $product['noOfTickets'] = $result->quantity ?? 0;
+            $product['orderId'] = $orderId;
+            $product['order_id'] = $result->order_id;
+            $product['orderPlacedOn'] = $orderPlacedOn;
+            $product['paymentMethod'] = 'Credit Card';
+        }
+
+        $url = $this->productRepository->downloadTicket($product);
+        $response['url'] = $url;
+        return $response;
+    }
+    public function emailInvoice($rootValue, array $args, GraphQLContext $context)
+    {
+        $product = $this->productRepository->findOrFail($args['product_id']);
+        $customer = bagisto_graphql()->guard($this->guard)->user();
+        SendEventTicket::dispatch($customer);
+        if(!empty($customer)) {
+            $product['customerFirstName'] = $customer->first_name ?? null;
+            $product['customerLastName'] = $customer->last_name ?? null;
+            $product['customerEmail'] = $customer->email ?? null;
+            $product['customerPhone'] = $customer->phone ?? null;
+            $customerAddress = $this->customerAddressRepository->where([["customer_id", $customer->id], ['default_address', 1]])->first();
+            if(!empty($customerAddress)){
+                $product['customerAddressFirstName'] = $customerAddress['first_name'];
+                $product['customerAddressLastName'] = $customerAddress['last_name'];
+                $product['customerAddressEmail'] = $customerAddress['email'];
+                $product['customerAddress1'] = $customerAddress['address1'];
+                $product['customerAddress2'] = $customerAddress['address2'];
+                if($customer->phone == null) {
+                    $product['customerPhone'] = $customerAddress['phone'] ?? null;
+                }
+                $product['customerAddressCity'] = $customerAddress['city'];
+                $product['customerAddressState'] = $customerAddress['state'];
+                $product['customerAddressCountry'] = $customerAddress['country'];
+                $product['customerAddressPostCode'] = $customerAddress['postcode'];
+            }
+        }
+
+        $result = DB::table('orders')
+            ->leftJoin('cart_items', 'cart_items.cart_id', '=', 'orders.cart_id')
+            ->leftJoin('products', 'cart_items.product_id', '=', 'products.id')
+            ->addSelect('cart_items.quantity', 'orders.created_at', 'orders.id AS order_id')
+            ->where('products.type', 'booking')
+            ->where('orders.status', 'completed')
+            ->groupBy('cart_items.product_id')->first();
+
+        if(isset($result)) {
+            $orderPlacedOn = null;
+            if(isset($result->created_at))
+                $orderPlacedOn = date("F d, Y, H:i", strtotime($result->created_at));
+
+            $orderId = null;
+            if(isset($result->order_id))
+                $orderId = "#".$result->order_id;
+
+            $product['noOfTickets'] = $result->quantity ?? 0;
+            $product['orderId'] = $orderId;
+            $product['order_id'] = $result->order_id;
+            $product['orderPlacedOn'] = $orderPlacedOn;
+            $product['paymentMethod'] = 'Credit Card';
+        }
+
+        $url = $this->productRepository->downloadTicket($product);
+        $response['url'] = $url;
+        return $response;
     }
 }
