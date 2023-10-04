afc16d7af388bbe80cf9a0e31de3b9169865baad
tushar-addweb <tushar@addwebsolution.in>
2023-10-04 18:06:37 +0530

    My orders and ticket screen download

--- src/Mutations/Shop/Product/ProductMutation.php
+++ src/Mutations/Shop/Product/ProductMutation.php
@@ -1308,20 +1308,54 @@ class ProductMutation extends Controller
 
     public function getBookedTicketListByCustomer($rootValue, array $args, GraphQLContext $context)
     {
+
+
+        $product = [];
+
+//        $query = \Webkul\GraphQLAPI\Models\Catalog\Product::query();
+//        $query
+//            ->join('booked_event_tickets_history', 'booked_event_tickets_history.product_id', '=', 'products.id')
+//            ->join('orders', 'booked_event_tickets_history.orderId', '=', 'orders.id')
+//            ->select('products.*','booked_event_tickets_history.product_id','orders.id as order_id','orders.customer_email as email','orders.customer_first_name as first_name','orders.customer_last_name as last_name','booked_event_tickets_history.id as orderedTicketId','booked_event_tickets_history.qrCode','booked_event_tickets_history.is_checkedIn','booked_event_tickets_history.ticket_id','orders.created_at','orders.updated_at')
+//            ->where('orders.id',$args['order_id'])
+//            ->where('booked_event_tickets_history.orderId',$args['order_id'])
+//            ->where('orders.customer_email',$customer->email)
+//            ->whereNotNull('products.id')
+//            ->where('products.type', 'booking')
+//            ->groupBy('booked_event_tickets_history.product_id')
+//            ->orderBy('booked_event_tickets_history.product_id','desc');
+//        $count = isset($args['first']) ? $args['first'] : 10;
+//        $page = isset($args['page']) ? $args['page'] : 1;
+//        return $query->paginate($count, ['*'], 'page', $page);
+   $query = \Webkul\GraphQLAPI\Models\Catalog\Product::query();
+//        $query
+//            ->join('booked_event_tickets_history', 'booked_event_tickets_history.product_id', '=', 'products.id')
+//            ->join('orders', 'booked_event_tickets_history.orderId', '=', 'orders.id')
+//            ->select('products.*','booked_event_tickets_history.product_id','orders.id as order_id','orders.customer_email as email','orders.customer_first_name as first_name','orders.customer_last_name as last_name','booked_event_tickets_history.id as orderedTicketId','booked_event_tickets_history.qrCode','booked_event_tickets_history.is_checkedIn','booked_event_tickets_history.ticket_id','orders.created_at','orders.updated_at')
+//            ->where('orders.id',$args['order_id'])
+//            ->where('booked_event_tickets_history.orderId',$args['order_id'])
+//            ->where('orders.customer_email',$customer->email)
+//            ->whereNotNull('products.id')
+//            ->where('products.type', 'booking')
+//            ->groupBy('booked_event_tickets_history.product_id')
+//            ->orderBy('booked_event_tickets_history.product_id','desc');
+//        $count = isset($args['first']) ? $args['first'] : 10;
+//        $page = isset($args['page']) ? $args['page'] : 1;
+//        return $query->paginate($count, ['*'], 'page', $page);
+
+
         $owner = bagisto_graphql()->guard($this->guard)->user();
-        $prefix = DB::getTablePrefix();
+
         $query = \Webkul\GraphQLAPI\Models\Catalog\Product::query()
-            ->leftJoin('cart_items', 'products.id', '=', 'cart_items.product_id')
-            ->leftJoin('orders', 'cart_items.cart_id', '=', 'orders.cart_id')
+            ->leftJoin('booked_event_tickets_history', 'booked_event_tickets_history.product_id', '=', 'products.id')
+            ->leftJoin('orders', 'booked_event_tickets_history.orderId', '=', 'orders.id')
             ->leftJoin('addresses', 'orders.customer_email', '=', 'addresses.email')
-            ->addSelect('products.id', 'products.sku as productName', 'orders.created_at', 'cart_items.ticket_id', 'orders.id AS order_id', 'addresses.address_type', 'addresses.first_name as firstname', 'addresses.last_name  as lastname', 'addresses.address1', 'addresses.address2', 'addresses.postcode', 'addresses.city', 'addresses.state', 'addresses.country', 'addresses.email', 'addresses.phone', 'cart_items.total as price', 'orders.status', 'cart_items.base_price as basePrice')
-            ->selectRaw('SUM(' . $prefix . 'cart_items.quantity) as quantity')
-            ->whereIn('orders.status', ['completed', 'pending'])
-//            ->where('addresses.default_address', 1)
+            ->addSelect('products.*', 'products.sku as productName',  'booked_event_tickets_history.ticket_id', 'orders.id AS order_id', 'addresses.address_type', 'addresses.first_name as firstname', 'addresses.last_name  as lastname', 'addresses.address1', 'addresses.address2', 'addresses.postcode', 'addresses.city', 'addresses.state', 'addresses.country', 'addresses.email', 'addresses.phone',  'orders.status','booked_event_tickets_history.id as orderedTicketId','booked_event_tickets_history.qrCode','booked_event_tickets_history.is_checkedIn','booked_event_tickets_history.ticket_id','orders.created_at as orderDate')
             ->where('products.type', 'booking')
             ->where('orders.customer_email', $owner->email)
-            ->groupBy('cart_items.product_id', 'cart_items.cart_id')
-            ->orderBy('orders.id', 'desc');
+            ->groupBy('booked_event_tickets_history.id')
+            ->orderBy('booked_event_tickets_history.id', 'desc');
+
         $count = isset($args['first']) ? $args['first'] : 10;
         $page = isset($args['page']) ? $args['page'] : 1;
         return $query->paginate($count, ['*'], 'page', $page);
